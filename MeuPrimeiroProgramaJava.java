9372844046f10c7ee190db071e7793ab431519ec
wldmrntt <79091320+wldmrntt@users.noreply.github.com>
2023-04-11 14:53:11 -0300

    Projeto Completo
    Projeto introdutório de uso para o Java.

---
+++ MeuPrimeiroProgramaJava.java
@@ -0,0 +1,23 @@
+//salvar como MeuPrimeiroProgramaJava.java
+
+//nome da classe
+class MeuPrimeiroProgramaJava
+{
+	//modulo principa com a entrada pela linha de comando
+	public static void main (String entrada[])
+	{
+		//declaracao de variaveis
+			int inteiro = 47;
+			char caracter = 'F';
+			double real = 1.65;
+			String frase = "Lucy Mari ";
+			boolean VF=true;
+
+			if (VF == true)
+			{
+			System.out.printIn("Eu sou a " + frase + "tenho " + inteiro + "anos e tenho " + real + "m de altura");
+			}
+
+			System.exit(0);
+	}
+}
